b907b623e19a55614968a38a0551aa6331ae613c
Pulse <Pulse@pulse.com>
2020-02-27 20:34:07 +0300

    27.02.20

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,32 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></img></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></img></button>'
-    });
-  });
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></img></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></img></button>',
+//         responsive: [
+//         {
+//             breakpoint: 968,
+//             settings: {
+//               dots: true,
+//               arrows: false
+//             }
+//         }
+//         ]
+//     });
+//   });
+
+const slider = tns ({
+	container: '.carousel__inner',
+	items: 1,
+	slideBy: 'page',
+	autoplay: false,
+	controls: false,
+	nav: false
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+	slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+	slider.goTo('next');
+});
